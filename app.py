9a38047dd4e8a8bc5367560102b0dd91acdb5b43
Jordan Hewitt <1633901+src-r-r@users.noreply.github.com>
2020-01-30 11:00:05 -0800

    fix path issue.

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask,jsonify, render_template
+from flask import Flask, jsonify, render_template
 
 from flask_restful import Api
 from flask_jwt_extended import JWTManager
@@ -6,16 +6,22 @@ from flask_jwt_extended import JWTManager
 from tt.model import dbt
 from tt import resources
 
-#app = Flask(__name__, static_url_path='/build')
-app = Flask(__name__.split('.')[0], static_folder='client/build/static', template_folder="client/public")
+# app = Flask(__name__, static_url_path='/build')
+app = Flask(
+    __name__.split(".")[0],
+    static_folder="client/build/static",
+    template_folder="client/build",
+)
 api = Api(app)
 
-app.config['JWT_SECRET_KEY'] = 'boost-is-the-secret-of-our-app'
-jwt=JWTManager(app)
+app.config["JWT_SECRET_KEY"] = "boost-is-the-secret-of-our-app"
+jwt = JWTManager(app)
 
-@app.route('/')
+
+@app.route("/")
 def index():
-    return render_template('index.html')
+    return render_template("index.html")
+
 
 # api.add_resource(resources.UserRegistration, '/register')
 # api.add_resource(resources.UserLogin, '/login')
