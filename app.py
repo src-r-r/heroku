6307349d700a957d3cf514d040929bcd86439405
jeff1996219 <jeff1996219@hotmail.com>
2020-01-30 17:44:52 +0200

    ss

--- app.py
+++ app.py
@@ -17,8 +17,8 @@ jwt=JWTManager(app)
 def index():
     return render_template('index.html')
 
-api.add_resource(resources.UserRegistration, '/register')
-api.add_resource(resources.UserLogin, '/login')
+# api.add_resource(resources.UserRegistration, '/register')
+# api.add_resource(resources.UserLogin, '/login')
 
 if __name__ == "__main__":
     app.debug = True
