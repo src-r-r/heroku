5141547678f595b164483dbe1d6230c77794f004
jeff1996219 <jeff1996219@hotmail.com>
2020-01-30 17:52:36 +0200

    ss

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ from tt.model import dbt
 from tt import resources
 
 #app = Flask(__name__, static_url_path='/build')
-app = Flask(__name__.split('.')[0], static_folder='client/build/static', template_folder="client/build")
+app = Flask(__name__.split('.')[0], static_folder='/client/build/static', template_folder="/client/build")
 api = Api(app)
 
 app.config['JWT_SECRET_KEY'] = 'boost-is-the-secret-of-our-app'
